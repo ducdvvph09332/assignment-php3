b84b5bb35cdb24cf442065e1164d9d5f4cea46da
Việt Đức <ducdvvph09332@fpt.edu.vn>
2020-10-07 21:04:18 +0700

    first commit

---
+++ app/Models/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+    protected $table = 'posts';
+
+    protected $fillable = [
+        'desc',
+        'content',
+        'image_url',
+    ];
+
+    public function comments(){
+        return $this->hasMany(Comment::class, 'post_id', 'id');
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use App\Models\CategoryPost;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\Student;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        Student::factory(20)->create();
+        Post::factory(10)->create();
+        Comment::factory(10)->create();
+        Category::factory(10)->create();
+        CategoryPost::factory(10)->create();
+    }
+}
